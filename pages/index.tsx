489e6d6efd86b86175e47ad04a0e770009c58601
andres9888 <andres9888@gmail.com>
2021-02-18 12:35:46 -0500

    add more descriptions

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,131 +12,42 @@ const Home = () => (
     </Head>
 
     <Nav />
-    <h1>working</h1>
     <div className="container">
+    <h1>Resources for Studying Japanese</h1>
     <table className="table">
   <thead>
     <tr>
-      <th><abbr title="Position">Pos</abbr></th>
-      <th>Team</th>
-      <th><abbr title="Played">Pld</abbr></th>
-      <th><abbr title="Won">W</abbr></th>
-      <th><abbr title="Drawn">D</abbr></th>
-      <th><abbr title="Lost">L</abbr></th>
-      <th><abbr title="Goals for">GF</abbr></th>
-      <th><abbr title="Goals against">GA</abbr></th>
-      <th><abbr title="Goal difference">GD</abbr></th>
-      <th><abbr title="Points">Pts</abbr></th>
-      <th>Qualification or relegation</th>
+      <th><abbr title="Position">Image</abbr></th>
+      <th><abbr title="Played">Title</abbr></th>
+      <th><abbr title="Won">Description</abbr></th>
+      <th><abbr title="Drawn">Tags</abbr></th>
+      <th><abbr title="Lost">Actions</abbr></th>
     </tr>
   </thead>
   <tfoot>
+  <thead><tr><th><h1>Tools</h1></th></tr></thead>
     <tr>
-      <th><abbr title="Position">Pos</abbr></th>
-      <th>Team</th>
-      <th><abbr title="Played">Pld</abbr></th>
-      <th><abbr title="Won">W</abbr></th>
-      <th><abbr title="Drawn">D</abbr></th>
-      <th><abbr title="Lost">L</abbr></th>
-      <th><abbr title="Goals for">GF</abbr></th>
-      <th><abbr title="Goals against">GA</abbr></th>
-      <th><abbr title="Goal difference">GD</abbr></th>
-      <th><abbr title="Points">Pts</abbr></th>
-      <th>Qualification or relegation</th>
+    <th><abbr title="Position">Image</abbr></th>
+      <th><abbr title="Played">Tae Kim's Guide to Learning Japanese </abbr></th>
+      <th><abbr title="Won">Description</abbr>(good for supplementary learning/explanations)</th>
+      <th><abbr title="Drawn">Tags</abbr></th>
+      <th><abbr title="Lost">Actions</abbr></th>
     </tr>
   </tfoot>
   <tbody>
     <tr>
-      <th>1</th>
-      <td><a href="https://en.wikipedia.org/wiki/Leicester_City_F.C." title="Leicester City F.C.">Leicester City</a> <strong>(C)</strong>
-      </td>
-      <td>38</td>
-      <td>23</td>
-      <td>12</td>
-      <td>3</td>
-      <td>68</td>
-      <td>36</td>
-      <td>+32</td>
-      <td>81</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
+    <td><abbr title="Position">Image</abbr></td>
+      <td><abbr title="Played">Title</abbr></td>
+      <td><abbr title="Won">Description</abbr></td>
+      <td><abbr title="Drawn">Tags</abbr></td>
+      <td><abbr title="Lost">Actions</abbr></td>
     </tr>
-    <tr>
-      <th>2</th>
-      <td><a href="https://en.wikipedia.org/wiki/Arsenal_F.C." title="Arsenal F.C.">Arsenal</a></td>
-      <td>38</td>
-      <td>20</td>
-      <td>11</td>
-      <td>7</td>
-      <td>65</td>
-      <td>36</td>
-      <td>+29</td>
-      <td>71</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
-    </tr>
-    <tr>
-      <th>3</th>
-      <td><a href="https://en.wikipedia.org/wiki/Tottenham_Hotspur_F.C." title="Tottenham Hotspur F.C.">Tottenham Hotspur</a></td>
-      <td>38</td>
-      <td>19</td>
-      <td>13</td>
-      <td>6</td>
-      <td>69</td>
-      <td>35</td>
-      <td>+34</td>
-      <td>70</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
-    </tr>
-    <tr class="is-selected">
-      <th>4</th>
-      <td><a href="https://en.wikipedia.org/wiki/Manchester_City_F.C." title="Manchester City F.C.">Manchester City</a></td>
-      <td>38</td>
-      <td>19</td>
-      <td>9</td>
-      <td>10</td>
-      <td>71</td>
-      <td>41</td>
-      <td>+30</td>
-      <td>66</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Play-off_round" title="2016–17 UEFA Champions League">Champions League play-off round</a></td>
-    </tr>
-    <tr>
-      <th>5</th>
-      <td><a href="https://en.wikipedia.org/wiki/Manchester_United_F.C." title="Manchester United F.C.">Manchester United</a></td>
-      <td>38</td>
-      <td>19</td>
-      <td>9</td>
-      <td>10</td>
-      <td>49</td>
-      <td>35</td>
-      <td>+14</td>
-      <td>66</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Europa_League#Group_stage" title="2016–17 UEFA Europa League">Europa League group stage</a></td>
-    </tr>
-    <tr>
-      <th>6</th>
-      <td><a href="https://en.wikipedia.org/wiki/Southampton_F.C." title="Southampton F.C.">Southampton</a></td>
-      <td>38</td>
-      <td>18</td>
-      <td>9</td>
-      <td>11</td>
-      <td>59</td>
-      <td>41</td>
-      <td>+18</td>
-      <td>63</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Europa_League#Group_stage" title="2016–17 UEFA Europa League">Europa League group stage</a></td>
-    </tr>
-    <tr>
-      <th>7</th>
-      <td><a href="https://en.wikipedia.org/wiki/West_Ham_United_F.C." title="West Ham United F.C.">West Ham United</a></td>
-      <td>38</td>
-      <td>16</td>
-      <td>14</td>
-      <td>8</td>
-      <td>65</td>
-      <td>51</td>
-      <td>+14</td>
-      <td>62</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Europa_League#Third_qualifying_round" title="2016–17 UEFA Europa League">Europa League third qualifying round</a></td>
+    <tr className="is-selected">
+    <td><abbr title="Position">Image</abbr></td>
+      <td><abbr title="Played">Title</abbr></td>
+      <td><abbr title="Won">Description</abbr></td>
+      <td><abbr title="Drawn">Tags</abbr></td>
+      <td><abbr title="Lost">Actions</abbr></td>
     </tr>
   </tbody>
 </table>
