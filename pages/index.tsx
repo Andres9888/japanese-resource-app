84afa13fd8c1f05e40b70f5549da987d2d85751d
andres9888 <andres9888@gmail.com>
2021-02-19 11:32:53 -0500

    -add ulr and image data to jpresource.json - set up styled components on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '~views/components/Nav'
 import jpresources from 'data/jpresources.json'
+import styled from 'styled-components'
+
+const TableRow = styled.tr`
+  
+`;
+
+const TableData = styled.td`
+  img{
+    width: 300px;
+  }
+`;
+
+
 
 const Home = ({ props }) => {
   return (
@@ -18,7 +31,7 @@ const Home = ({ props }) => {
       <Nav />
       <div className='container'>
         <h1>Resources for Studying Japanese</h1>
-        <table className='table'>
+        <table className='table is-fullwidth is-hoverable'>
           <thead>
             <tr>
               <th>Image</th>
@@ -29,14 +42,14 @@ const Home = ({ props }) => {
             </tr>
           </thead>
           <tbody>
-            {props.content.map((item, index) => (
-              <tr>
-              <th>Image</th>
-              <th>{item.title}</th>
-              <th>{item.description}</th>
-              <th>Tags</th>
-              <th>Actions</th>
-            </tr>
+            {props.content.map((resource, index) => (
+              <TableRow>
+              <TableData><img src={resource.image} alt=""/></TableData>
+              <TableData><a href={resource.url}>{resource.title}</a></TableData>
+              <TableData>{resource.description}</TableData>
+              <TableData>Tags</TableData>
+              <TableData>Actions</TableData>
+            </TableRow>
             ))}
           </tbody>
         </table>
