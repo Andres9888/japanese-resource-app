171d5bae3972ebf37ca3e02b6ccd253c5b03d0fd
andres9888 <andres9888@gmail.com>
2021-02-18 23:35:58 -0500

    add data from json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,134 @@
 import React from 'react'
 import Head from 'next/head'
 import Nav from '~views/components/Nav'
+import jpresources from 'data/jpresources.json'
 
-
-const Home = () => (
+const Home = ({stars}) => {
+  console.log(stars)
+  return (
   <div>
     <Head>
       <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" />
+      <link rel='icon' href='/favicon.ico' />
+      <link
+        rel='stylesheet'
+        href='https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css'
+      />
     </Head>
 
     <Nav />
-    <div className="container">
-    <h1>Resources for Studying Japanese</h1>
-    <table className="table">
-  <thead>
-    <tr>
-      <th><abbr title="Position">Image</abbr></th>
-      <th><abbr title="Played">Title</abbr></th>
-      <th><abbr title="Won">Description</abbr></th>
-      <th><abbr title="Drawn">Tags</abbr></th>
-      <th><abbr title="Lost">Actions</abbr></th>
-    </tr>
-  </thead>
-  <tfoot>
-  <thead><tr><th><h1>Tools</h1></th></tr></thead>
-    <tr>
-    <th><abbr title="Position">Image</abbr></th>
-      <th><abbr title="Played">Tae Kim's Guide to Learning Japanese </abbr></th>
-      <th><abbr title="Won">Description</abbr>(good for supplementary learning/explanations)</th>
-      <th><abbr title="Drawn">Tags</abbr></th>
-      <th><abbr title="Lost">Actions</abbr></th>
-    </tr>
-  </tfoot>
-  <tbody>
-    <tr>
-    <td><abbr title="Position">Image</abbr></td>
-      <td><abbr title="Played">Title</abbr></td>
-      <td><abbr title="Won">Description</abbr></td>
-      <td><abbr title="Drawn">Tags</abbr></td>
-      <td><abbr title="Lost">Actions</abbr></td>
-    </tr>
-    <tr className="is-selected">
-    <td><abbr title="Position">Image</abbr></td>
-      <td><abbr title="Played">Title</abbr></td>
-      <td><abbr title="Won">Description</abbr></td>
-      <td><abbr title="Drawn">Tags</abbr></td>
-      <td><abbr title="Lost">Actions</abbr></td>
-    </tr>
-  </tbody>
-</table>
+    <div className='container'>
+      <h1>Resources for Studying Japanese</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>
+              <abbr title='Position'>Image</abbr>
+            </th>
+            <th>
+              <abbr title='Played'>Title</abbr>
+            </th>
+            <th>
+              <abbr title='Won'>Description</abbr>
+            </th>
+            <th>
+              <abbr title='Drawn'>Tags</abbr>
+            </th>
+            <th>
+              <abbr title='Lost'>Actions</abbr>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <abbr title='Position'>Image</abbr>
+            </th>
+            <th>
+              <abbr title='Played'>{stars.content[0].title}</abbr>
+            </th>
+            <th>
+              <abbr title='Won'>Description</abbr>
+            </th>
+            <th>
+              <abbr title='Drawn'>Tags</abbr>
+            </th>
+            <th>
+              <abbr title='Lost'>Actions</abbr>
+            </th>
+          </tr>
+        </thead>
+        <tfoot>
+          <thead>
+            <tr>
+              <th>
+                <h1>Tools</h1>
+              </th>
+            </tr>
+          </thead>
+          <tr>
+            <th>
+              <abbr title='Position'>Image</abbr>
+            </th>
+            <th>
+              <abbr title='Played'>Tae Kim's Guide to Learning Japanese </abbr>
+            </th>
+            <th>
+              <abbr title='Won'>Description</abbr>(good for supplementary
+              learning/explanations)
+            </th>
+            <th>
+              <abbr title='Drawn'>
+                
+              </abbr>
+            </th>
+            <th>
+              <abbr title='Lost'>Actions</abbr>
+            </th>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr>
+            <td>
+              <abbr title='Position'>Image</abbr>
+            </td>
+            <td>
+              <abbr title='Played'>Title</abbr>
+            </td>
+            <td>
+              <abbr title='Won'>Description</abbr>
+            </td>
+            <td>
+              <abbr title='Drawn'>Tags</abbr>
+            </td>
+            <td>
+              <abbr title='Lost'>Actions</abbr>
+            </td>
+          </tr>
+          <tr className='is-selected'>
+            <td>
+              <abbr title='Position'>Image</abbr>
+            </td>
+            <td>
+              <abbr title='Played'>Title</abbr>
+            </td>
+            <td>
+              <abbr title='Won'>Description</abbr>
+            </td>
+            <td>
+              <abbr title='Drawn'>Tags</abbr>
+            </td>
+            <td>
+              <abbr title='Lost'>Actions</abbr>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-  </div>
-)
+)}
 
+Home.getInitialProps = async (ctx) => {
+  
+  return { stars: jpresources }
+
+}
 export default Home
