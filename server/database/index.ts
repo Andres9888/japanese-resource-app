487de4ccd3d7b210fc632cef7c977849b1b7ec24
Andres9888 <Andres9888@Gmail.com>
2021-05-09 09:29:18 -0400

    remove unneed commets from database index

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,26 +1,18 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb'
 
-// Note: Need to add appropriate credentials here to make the connection
-// Note #2: Database credentials should never be committed to source code!
-// const user = hidden.USER;
-// const userPassword = hidden.USERPASSWORD;
-// const cluster = hidden.CLUSTER;
 const dbname = process.env.DBNAME
 
-//const url = `mongodb+srv://${user}:${userPassword}@${cluster}/${dbname}?retryWrites=true&w=majority`;
-
 const url = encodeURI(process.env.DBURL)
-
 
 export const connectDatabase = async () => {
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  const db = client.db(dbname);
+    useUnifiedTopology: true,
+  })
+  const db = client.db(dbname)
 
   return {
-    listings: db.collection("japanese-resources-collection"),
-    users: db.collection('users')
-  };
-};
+    listings: db.collection('japanese-resources-collection'),
+    users: db.collection('users'),
+  }
+}
