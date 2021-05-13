4cc240f9fd8f4aaa0c5409e5c5bb0c843fdca57c
Andres9888 <Andres9888@Gmail.com>
2021-05-13 15:31:05 -0400

    Nav with searchbar

--- views/components/Nav.tsx
+++ views/components/Nav.tsx
@@ -1,36 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from './nav.module.scss'
-import { Layout } from "antd";
+import { Layout, Avatar, Affix, Input, Button, Icon, Menu } from 'antd'
 
-const { Header } = Layout;
+const { Header } = Layout
+const { Search } = Input
+const { Item, SubMenu } = Menu
 
-const Nav = ({viewer}) =>(
-  <Header className="app-header">
-      <div className="app-header__logo-search-section">
-      </div>
-      <div className="app-header__menu-section">
-      <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/login">
-          <a>Login Page</a>
-        </Link>
-      </li>
-      <li>
-        <img src={viewer ? viewer.avatar : ""} alt=""/>
-      </li>
-    </ul>
-  </nav>
-      </div>
-    </Header>
-)
+const Nav = ({ viewer }) =>(
+    <Affix offsetTop={0} className='app__affix-header'>
+      <Header className='app-header'>
+        <div className='app-header__logo-search-section'>
+          <div className='app-header__logo'></div>
+          <div className='app-header__search-input'>
+            <Search placeholder="Search 'San Fransisco'"/>
+          </div>
+        </div>
+      </Header>
+    </Affix>)
+    
+
 
 export default Nav
-
-
