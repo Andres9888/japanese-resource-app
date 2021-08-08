2f344c2f0a1c2f0baf9405fa522de00ed5f8f2ff
Andres9888 <Andres9888@Gmail.com>
2021-08-07 20:30:02 -0400

    format

--- views/components/VoteButton.tsx
+++ views/components/VoteButton.tsx
@@ -5,43 +5,49 @@ import { INCREMENT_COUNT } from '~graphql/mutations/mutations'
 import { initializeApollo } from '~lib/apolloClient'
 
 const VoteButton = ({ resource, viewer, refetch }) => {
-    const [incrementCount] = useMutation(INCREMENT_COUNT)
-    const [disabled, setDisabled] = useState(false);
+  const [incrementCount] = useMutation(INCREMENT_COUNT)
+  const [disabled, setDisabled] = useState(false)
 
-    const handleIncrementCount = async resource => {
-        if (viewer.id) {
-            const client = initializeApollo()
-            const {
-                data: { checkUserVote: didVote }
-            } = await client.query({
-                query: CHECK_USER_VOTE,
-                variables: {
-                    id: viewer.id,
-                    resource: resource.id,
-                },
-            })
+  const handleIncrementCount = async resource => {
+    if (viewer.id) {
+      const client = initializeApollo()
+      const {
+        data: { checkUserVote: didVote },
+      } = await client.query({
+        query: CHECK_USER_VOTE,
+        variables: {
+          id: viewer.id,
+          resource: resource.id,
+        },
+      })
 
-            if (didVote.length === 0) {
-                await incrementCount({
-                    variables: {
-                        id: resource.id,
-                        viewer: viewer.id,
-                        resource: resource.id,
-                    },
-                })
-                setDisabled(!disabled);
-                refetch()
-            } else {
-                alert('already voted on this resource')
-            }
-        } else {
-            alert('most login to vote')
-        }
+      if (didVote.length === 0) {
+        await incrementCount({
+          variables: {
+            id: resource.id,
+            viewer: viewer.id,
+            resource: resource.id,
+          },
+        })
+        setDisabled(!disabled)
+        refetch()
+      } else {
+        alert('already voted on this resource')
+      }
+    } else {
+      alert('most login to vote')
     }
-    return (
-    <button disabled={disabled} onClick={() => {handleIncrementCount(resource)}}>
-        👍
-    </button>)
+  }
+  return (
+    <button
+      disabled={disabled}
+      onClick={() => {
+        handleIncrementCount(resource)
+      }}
+    >
+      👍
+    </button>
+  )
 }
 
 export default VoteButton
