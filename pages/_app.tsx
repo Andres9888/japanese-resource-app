42ff5ffed49a44d87377cab85998fab3414a4535
Andres9888 <Andres9888@Gmail.com>
2021-09-14 11:37:43 -0400

    get initial donation button setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 //@ts-nocheck
-import React, { useState } from 'react'
-import { ApolloProvider } from '@apollo/react-hooks'
-import { useApollo } from '~lib/apolloClient'
-import '~styles/main.scss'
-import { Viewer } from '~@types/globalTypes'
+import React, { useState } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useApollo } from '~lib/apolloClient';
+import '~styles/main.scss';
+import { Viewer } from '~@types/globalTypes';
+import Head from 'next/head';
 
 const initialViewer: Viewer = {
   id: null,
@@ -11,14 +12,25 @@ const initialViewer: Viewer = {
   avatar: null,
   hasWallet: null,
   didRequest: false,
-}
+};
 
 export default function App({ Component, pageProps }) {
-  const [viewer, setViewer] = useState<Viewer>(initialViewer)
-  const apolloClient = useApollo(pageProps.initialApolloState)
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const apolloClient = useApollo(pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
+      <Head>
+        <script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"></script>
+        <script>
+          {kofiWidgetOverlay.draw('andres9888', {
+            'type': 'floating-chat',
+            'floating-chat.donateButton.text': 'Support me',
+            'floating-chat.donateButton.background-color': '#1890ff',
+            'floating-chat.donateButton.text-color': '#fff',
+          })}
+        </script>
+      </Head>
       <Component {...pageProps} viewer={viewer} setViewer={setViewer} />
     </ApolloProvider>
-  )
+  );
 }
