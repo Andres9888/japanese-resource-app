2ad5196403f24dcb2db1dcb4529457a98a0e9a75
Andres9888 <Andres9888@Gmail.com>
2021-10-24 20:02:30 -0400

    initial recommendation engine working

--- pages/api/recommendation.ts
+++ pages/api/recommendation.ts
@@ -24,9 +24,8 @@ export default async (_req, res) => {
         let review = [];
         review.push(element._id);
         resources.forEach(resource => {
-          console.log(element.resources);
-          console.log(resource, 'this is a resource');
-          if (element.resources.includes(`${resource}`)) {
+          if (element.resources === undefined) {
+          } else if (element.resources.includes(`${resource}`)) {
             review.push(1);
           } else {
             review.push(0);
@@ -42,26 +41,20 @@ export default async (_req, res) => {
 
     const reviewData = makeReview();
 
+    const stringResources = resources.map(element => JSON.stringify(element));
+
+    console.log([['empty', ...stringResources], ...reviewData]);
+
     const kNNRecommender = new KNNRecommender([
-      [
-        'emptycorner',
-        'item 1',
-        'item 2',
-        'item 3',
-        'item 4',
-        'item 5',
-        'item 6',
-        'item 7',
-      ],
-      ['user 1', 1, -1, 0, 0, -1, 1, 0],
-      ['user 2', 1, -1, 0, 1, -1, 0, 0],
+      ['emptycorner', ...stringResources],
+      ...reviewData,
     ]);
     kNNRecommender.initializeRecommender().then(() => {
       const userRecommendations = kNNRecommender.generateNNewUniqueRecommendationsForUserId(
-        'user 2'
+        '101486254571069117979'
       );
       console.log(
-        `new recommendation for user 2 ${userRecommendations[0].itemId}`
+        `new recommendation for 101486254571069117979 ${userRecommendations[0].itemId}`
       );
     });
     res.status(200).json({ resources, users, reviewData });
