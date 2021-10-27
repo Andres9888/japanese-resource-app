21f787acaa60a554d7edde91e188e89274c397fa
Andres9888 <Andres9888@Gmail.com>
2021-10-27 14:01:22 -0400

    add sakura.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,6 +70,33 @@ export default function Home({ viewer }: Props) {
           });
         }}
       />
+      <Script
+        src="/static/scripts/sakura.min.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          // @ts-ignore
+          var sakura = new Sakura('body', {
+            colors: [
+              {
+                gradientColorStart: 'rgba(255, 183, 197, 0.9)',
+                gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
+                gradientColorDegree: 120,
+              },
+              {
+                gradientColorStart: 'rgba(255,189,189)',
+                gradientColorEnd: 'rgba(227,170,181)',
+                gradientColorDegree: 120,
+              },
+              {
+                gradientColorStart: 'rgba(212,152,163)',
+                gradientColorEnd: 'rgba(242,185,196)',
+                gradientColorDegree: 120,
+              },
+            ],
+          });
+        }}
+      />
+
       <Nav
         handleChange={handleChange}
         searchTerm={searchTerm}
