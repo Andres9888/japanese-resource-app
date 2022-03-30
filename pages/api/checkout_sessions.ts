6936292c643ffdca59daf360329efd6643f078ac
Andres9888 <Andres9888@Gmail.com>
2022-03-30 13:18:10 -0400

    initial setup for custom stripe chekcout

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -4,27 +4,67 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    let paymentIntent;
+    let customer;
     try {
-      // Create Checkout Sessions from body params.
-      const database = await connectDatabase();
-      const users = await database.users.find({}).toArray();
+      // You need to attach the PaymentMethod to a Customer in order to reuse
+      // Since we are using test cards, create a new Customer here
+      // You would do this in your payment flow that saves cards
+      customer = await stripe.customers.create({
+        payment_method_types: ['bancontact', 'card', 'ideal'],
+      });
 
-      const customer = await stripe.customers.create({
-        email: 'jenny.rosen@example.com',
-      });
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        mode: 'setup',
+      // List the customer's payment methods to find one to charge
+      const paymentMethods = await stripe.paymentMethods.list({
         customer: customer.id,
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        type: 'card',
       });
-      res.redirect(303, session.url);
+
+      // Create and confirm a PaymentIntent with the order amount, currency,
+      // Customer and PaymentMethod ID
+      paymentIntent = await stripe.paymentIntents.create({
+        amount: 100,
+        currency: 'usd',
+        payment_method: paymentMethods.data[0].id,
+        customer: customer.id,
+        off_session: true,
+        confirm: true,
+      });
+
+      res.status(200).json({
+        succeeded: true,
+        clientSecret: paymentIntent.client_secret,
+        publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      });
     } catch (error) {
-      res.status(error.statusCode || 500).json(error.message);
+      if (error.code === 'authentication_required') {
+        // Bring the customer back on-session to authenticate the purchase
+        // You can do this by sending an email or app notification to let them know
+        // the off-session purchase failed
+        // Use the PM ID and client_secret to authenticate the purchase
+        // without asking your customers to re-enter their details
+        res.status(400).json({
+          error: 'authentication_required',
+          paymentMethod: error.raw.payment_method.id,
+          clientSecret: error.raw.payment_intent.client_secret,
+          publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+          amount: 100,
+          card: {
+            brand: error.raw.payment_method.card.brand,
+            last4: error.raw.payment_method.card.last4,
+          },
+        });
+      } else if (error.code) {
+        // The card was declined for other reasons (e.g. insufficient funds)
+        // Bring the customer back on-session to ask them for a new payment method
+        res.status(400).send({
+          error: error.code,
+          clientSecret: error.raw.payment_intent.client_secret,
+          publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        });
+      } else {
+        console.log('Unknown error occurred', error);
+      }
     }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
   }
 }
--- pages/stripe.tsx
+++ pages/stripe.tsx
@@ -1,45 +1,46 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-import { CardElement, Elements, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
+
+import SetupForm from '~common/components/SetupForm';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe('pk_test_51KhIeyBb7SW2HKTCYBSUyXDid0B9Wf9j6p6BZLzFDGR4F040zXV1ikmb7qEZ2R57Xi5MWj1juiM8psrpcexMN5VQ00STrPccDE');
 
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+function App() {
+  const [cs, setCs] = useState();
 
-  const handleSubmit = async event => {
-    event.preventDefault();
+  useEffect(() => {
+    const getCs = async () => {
+      try {
+        const { data: responseData } = await axios.post(`/api/checkout_sessions`);
 
-    if (elements == undefined) {
-      return;
-    }
+        const { client_secret: clientSecret } = responseData;
+        setCs(clientSecret);
+      } catch (error_) {
+        console.log(error_);
+      }
+    };
 
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-    });
+    getCs();
+  }, []);
+
+  const options = {
+    // passing the client secret obtained in step 2
+    client_secret: cs,
+    // Fully customizable with appearance API.
+    appearance: {
+      /* ... */
+    },
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement />
-      <button disabled={!stripe || !elements} type="submit">
-        Pay
-      </button>
-    </form>
-  );
-};
-
-const Stripe = () => {
-  return (
-    <Elements stripe={stripePromise}>
-      <CheckoutForm />
+    <Elements options={options} stripe={stripePromise}>
+      <SetupForm />
     </Elements>
   );
-};
-
-export default Stripe;
+}
+export default App;
