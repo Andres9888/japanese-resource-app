236e645f14ee6e803b1a193256f7602b5ec2e387
Andres9888 <Andres9888@Gmail.com>
2022-04-01 11:07:19 -0400

    Revert "working client intent api"
    This reverts commit 2328b7ceebe38f3f012c982ce8fd8a17f9bf5249.

--- pages/stripe.tsx
+++ pages/stripe.tsx
@@ -1,57 +1,46 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
+
+import SetupForm from '~common/components/SetupForm';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-export default function PreviewPage() {
-  React.useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-    if (query.get('success')) {
-      console.log('Order placed! You will receive an email confirmation.');
-    }
+const stripePromise = loadStripe('pk_test_51KhIeyBb7SW2HKTCYBSUyXDid0B9Wf9j6p6BZLzFDGR4F040zXV1ikmb7qEZ2R57Xi5MWj1juiM8psrpcexMN5VQ00STrPccDE');
 
-    if (query.get('canceled')) {
-      console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
-    }
+function App() {
+  const [cs, setCs] = useState();
+
+  useEffect(() => {
+    const getCs = async () => {
+      try {
+        const { data: responseData } = await axios.post(`/api/checkout_sessions`);
+
+        const { client_secret: clientSecret } = responseData;
+        setCs(clientSecret);
+      } catch (error_) {
+        console.log(error_);
+      }
+    };
+
+    getCs();
   }, []);
 
+  const options = {
+    // passing the client secret obtained in step 2
+    client_secret: cs,
+    // Fully customizable with appearance API.
+    appearance: {
+      /* ... */
+    },
+  };
+
   return (
-    <form action="/api/checkout_sessions" method="POST">
-      <section>
-        <button role="link" type="submit">
-          Checkout
-        </button>
-      </section>
-      <style jsx>
-        {`
-          section {
-            background: #ffffff;
-            display: flex;
-            flex-direction: column;
-            width: 400px;
-            height: 112px;
-            border-radius: 6px;
-            justify-content: space-between;
-          }
-          button {
-            height: 36px;
-            background: #556cd6;
-            border-radius: 4px;
-            color: white;
-            border: 0;
-            font-weight: 600;
-            cursor: pointer;
-            transition: all 0.2s ease;
-            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-          }
-          button:hover {
-            opacity: 0.8;
-          }
-        `}
-      </style>
-    </form>
+    <Elements options={options} stripe={stripePromise}>
+      <SetupForm />
+    </Elements>
   );
 }
+export default App;
