c307a8179438bc6808644717919a8766bdd3a14d
Andres Gutierrez <andres9888@gmail.com>
2022-05-13 14:49:38 -0400

    initial trpc setup

---
+++ pages/IndexPage.tsx
@@ -0,0 +1,13 @@
+import { trpc } from '~lib/utils/trpc';
+
+export default function IndexPage() {
+  const hello = trpc.useQuery(['hello', { text: 'client' }]);
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <p>{hello.data.greeting}</p>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
-// @ts-nocheck
+// @ts-nochecky
+
+import { withTRPC } from '@trpc/next';
+import { AppType } from 'next/dist/shared/lib/utils';
+
+import { AppRouter } from './api/trpc/[trpc]';
+
 import '~styles/main.scss';
 import { useState, useEffect, useRef } from 'react';
 
@@ -32,7 +38,7 @@ declare global {
   }
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
   const [searchTerm, setSearchTerm] = useState('');
@@ -117,3 +123,25 @@ export default function App({ Component, pageProps }: AppProps) {
     </ApolloProvider>
   );
 }
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/trpc` : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(App);
