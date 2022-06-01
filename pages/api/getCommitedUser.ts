2f4cef6c0877ed2f82b1f2ac972cde7ef776d544
Andres Gutierrez <andres9888@gmail.com>
2022-06-01 11:12:07 -0400

    initial aws connection

--- pages/api/getCommitedUser.ts
+++ pages/api/getCommitedUser.ts
@@ -1,6 +1,9 @@
 // @ts-nocheck
 /* eslint-disable */
-
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({ region: 'us-east-1' });
 import { PrismaClient } from '@prisma/client';
 
 import { getTwoDaysAgo, didlogYesterday } from '~lib/utils/timeFunctions';
@@ -39,6 +42,38 @@ export default async function handler(request, response) {
       const usersToCharge = getUsersToCharge();
 
       const userStripeIdsToCharge = usersToCharge.map(user => user.stripeId);
+
+      // Create an SQS service object
+      const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+      const parameters = {
+        MessageGroupId: 'TestGroup',
+        MessageAttributes: {
+          Title: {
+            DataType: 'String',
+            StringValue: 'The Whistler',
+          },
+          Author: {
+            DataType: 'String',
+            StringValue: 'John Grisham',
+          },
+          WeeksOn: {
+            DataType: 'Number',
+            StringValue: '6',
+          },
+        },
+        MessageBody: 'Information about current NY Times fiction bestseller for week of 12/11/2016.',
+        // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+        // MessageGroupId: "Group1",  // Required for FIFO queues
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/316703392763/chargeUsers.fifo',
+      };
+
+      sqs.sendMessage(parameters, function(error, data) {
+        if (error) {
+          console.log('Error', error);
+        } else {
+          console.log('Success', data.MessageId);
+        }
+      });
 
       response.status(200).json({ message: 'success', usersCharged: userStripeIdsToCharge, UsersChargedInfo: usersToCharge });
     } catch (error) {
