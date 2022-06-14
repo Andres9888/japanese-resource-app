1dbebc7c1d90f1fb5d7a2f3c071e6351d9300fce
Andres9888 <Andres9888@Gmail.com>
2022-06-14 11:49:58 -0400

    refactor: :art: Reorganize Data in Tests and Remove Commented Code

--- tests/unit/Commit.unit.test.tsx
+++ tests/unit/Commit.unit.test.tsx
@@ -12,53 +12,28 @@ import Commit from '~pages/commit';
 import '@testing-library/jest-dom';
 
 import { Viewer } from '~types/globalTypes';
-// create unit test for Commit component
-// include apollo mock client in this component
 
 afterEach(cleanup);
 describe('Commit component', () => {
   const apolloClient = initializeApollo();
 
-  const viewer: Viewer = {
-    id: '1',
-    isCommited: false,
-    token: null,
-    avatar: null,
-    hasWallet: null,
-    didRequest: false,
-    name: null,
-  };
-
   const setViewer = jest.fn();
   it('Should Render Yes Text when a viewer is not commited', () => {
+    const NonCommitedViewer: Viewer = {
+      id: '1',
+      isCommited: false,
+      token: null,
+      avatar: null,
+      hasWallet: null,
+      didRequest: false,
+      name: null,
+    };
     const { getByText } = render(
       <ApolloProvider client={apolloClient}>
-        <Commit setViewer={setViewer} viewer={viewer} />
+        <Commit setViewer={setViewer} viewer={NonCommitedViewer} />
       </ApolloProvider>
     );
+    screen.debug();
     expect(getByText(/Yes/i)).toBeInTheDocument();
   });
-
-  // it('should render the commit button', () => {
-  //   const { getByText } = render(<Commit setViewer={setViewer} viewer={viewer} />);
-  //   expect(getByText('Commit')).toBeInTheDocument();
-  // });
-  // it('should render the commit button if the user is commited', () => {
-  //   const { getByText } = render(<Commit setViewer={setViewer} viewer={{ ...viewer, isCommited: true }} />);
-  //   expect(getByText('Remove Commitment')).toBeInTheDocument();
-  // });
-  // it('should render the commit button if the user is not commited', () => {
-  //   const { getByText } = render(<Commit setViewer={setViewer} viewer={{ ...viewer, isCommited: false }} />);
-  //   expect(getByText('Commit')).toBeInTheDocument();
-  // });
-  // it('should set the commited value to true if the user clicks the commit button', () => {
-  //   const { getByText } = render(<Commit setViewer={setViewer} viewer={viewer} />);
-  //   getByText('Commit').click();
-  //   expect(viewer.isCommited).toBe(true);
-  // });
-  // it('should set the commited value to false if the user clicks the remove commitment button', () => {
-  //   const { getByText } = render(<Commit setViewer={setViewer} viewer={{ ...viewer, isCommited: true }} />);
-  //   getByText('Remove Commitment').click();
-  //   expect(viewer.isCommited).toBe(false);
-  // });
 });
